a8faa7302421d76c0dd7780877e76c07a9030119
ABOOBACAR <andalvision@gmail.com>
2021-06-19 22:01:43 +0100

    codage video

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
-
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+//import { YoutubeVideoPlayer } from '@ionic-native/youtube-video-player/ngx';
+import { SplashScreen } from '@ionic-native/splash-screen/ngx';
+import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { VideoPlayer } from '@ionic-native/video-player/ngx';
+import { File } from '@ionic-native/file/ngx';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    VideoPlayer,
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+  ],
+
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+//import { YoutubeVideoPlayer } from '@ionic-native/youtube-video-player/ngx';
+import { VideoPlayer, VideoOptions } from '@ionic-native/video-player/ngx';
+import {DomSanitizer} from '@angular/platform-browser';
 
 @Component({
   selector: 'app-tab1',
@@ -6,7 +9,29 @@ import { Component } from '@angular/core';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+  videoOptions: VideoOptions
+  constructor(private videoPlayer: VideoPlayer) {}
+  invokeVideoPlayer(){
+    this.videoOptions = {
+      volume: 0.7
+    };
+  }  
+  playOfflineVideo() {
+    this.videoPlayer.play('file:///../../assets/www/videos/soum.mp4').then(() => {
+      console.log('video finished');
+    }).catch(error => {
+      console.log(error);
+    });
+  }
+  playOnlineVideo() { 
+    this.videoPlayer.play('https://www.youtube.com/watch?v=nhrYbhQJ5Sg').then(() => {
+      console.log('video finished');
+    }).catch(error => {
+      console.log(error);
+    });
+  }
 
-  constructor() {}
-
+  closeVideoPlayer() {
+    this.videoPlayer.close();
+  }
 }
